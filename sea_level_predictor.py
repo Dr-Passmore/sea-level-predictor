b8a27001a0c1a18e6227410173b090b6d021b579
Phillip Passmore <phillipspassmore@gmail.com>
2022-09-25 14:08:34 +0100

    Sea level predictor complete
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -12,19 +12,30 @@ def draw_plot():
                 sealevel['CSIRO Adjusted Sea Level'])
     
     # Create first line of best fit
-    line1 = linregress(sealevel['Year'], sealevel['CSIRO Adjusted Sea Level'])
-    year = np.arange(sealevel['Year'].min(), 2050, 1)
-    level = year*line1.slope + line1.intercept
     
-    plt.plot(year, level)
+    
+    #line1 = linregress(sealevel['Year'], sealevel['CSIRO Adjusted Sea Level'])
+    year = np.arange(sealevel['Year'].min(), 2051, 1)
+    #level = year*line1.slope + line1.intercept
+    res = linregress(sealevel['Year'], sealevel['CSIRO Adjusted Sea Level'])
+    
+    plt.plot(year, res.intercept + res.slope* year, 'r')
+    '''
+    
+    line1 = linregress(sealevel['Year'],
+                sealevel['CSIRO Adjusted Sea Level'])
+    plt.plot(sealevel['Year'], line1.intercept + line1.slope*sealevel['Year'], 'r')
+    '''
     # Create second line of best fit
-    sealevel2000 = sealevel[sealevel['Year']>1999]
+    sealevel2000 = sealevel[sealevel['Year']>=2000]
     
     line2 = linregress(sealevel2000['Year'], sealevel2000['CSIRO Adjusted Sea Level'])
-    year2 = np.arange(sealevel2000['Year'].min(), 2050, 1)
+    year2 = np.arange(sealevel2000['Year'].min(), 2051, 1)
     level2 = year2*line2.slope + line2.intercept
     
     plt.plot(year2, level2)
+    
+    
     # Add labels and title
     plt.xlabel("Year")
     plt.ylabel("Sea Level (inches)")
